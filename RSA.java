c87f9a8c9614569c7a6a2d53d6a6c4386b3682de
池田 康平 <ikeda.kohei4477@gmail.com>
2019-04-05 16:35:42 +0900

    finish

--- RSA.java
+++ RSA.java
@@ -1,14 +1,11 @@
+package com.company;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class RSA {
+public class Main {
     public static final int ALPH_SUM=53;
     static final char[] alpha = {'A','B','C','D','E','F','G','H','I','J',
             'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
@@ -16,39 +13,43 @@ public class RSA {
             'k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '};//53 elements
     static List list = new ArrayList<Long>();
     public static void main(String[] args) {
-	// write your code here//
-        //p = 7 (prime number)
-        //q = 19 (prime number)
-        //e = 18 (the least common multiple)
-        //public key = 5 (1 < public key < e)
-        //secret key = 49 (public key * secret key = e * n + 1)
-//        long p=7;
-//        long q=19;
-////        long n=getPublicKey1(p,q);
-//        long e = lcm(p,q);
-//        System.out.println("e:"+e);
-//        long d = getSecretKey(e,(p-1)*(q-1));
-//        int[] numArray = getString();
-//        showNum(numArray);
-//        showString(numArray);
-//        long[] longArray = encrypt(numArray,e,p*q);
-//        showNum(longArray);
-//        int[] array = decrypt(longArray,d,p*q);
-//        showNum(array);
-//        showString(array);
-
-        long a= 111;
-        long b= 30;
-        System.out.println("d="+gcdEx(a,b));
-        System.out.println("x:"+x+" y:"+y);
+        if(args.length<1){
+            System.out.println("To Generate Public key, put \"-g\" as first argument.\n\n" +
+                    "To Encrypt, put \"-e\" as first argument and put \"pk(public key)\" as second arg and put \"n(p*q)\" as third arg.\n" +
+                    "Enter Plain text. Then you can get Encrypted text(as number array). \n\n" +
+                    "To Decrypt, put \"-d\" as first argument and put \"sk(secret key)\" as second arg and put \"n(p*q)\" as third arg\n" +
+                    "Enter Encrypted text(number array with \",\"). Then you can get Decrypted text.");
+        }else{
+            if(String.valueOf(args[0]).equals("-g")){
+                generate();
+                return;
+            }else if(String.valueOf(args[0]).equals("-e") && args.length==3){
+                long pk=Long.valueOf(args[1]);
+                long n=Long.valueOf(args[2]);
+                int[] numArray = getString();
+                showNum(numArray);
+                showString(numArray);
+                long[] longArray = encrypt(numArray,pk,n);
+                showNum(longArray);
+                showString(longArray);
+                return;
+            }else if(String.valueOf(args[0]).equals("-d") && args.length==3){
+                long sk=Long.valueOf(args[1]);
+                long n=Long.valueOf(args[2]);
+                long[] longArray = getNum();
+                int[] array = decrypt(longArray,sk,n);
+                showNum(array);
+                showString(array);
+                return;
+            }
+        }
     }
     public static long[] encrypt(int[] numArray, long e, long n){
         long[] longArray = new long[numArray.length];
         for(int i=0;i<numArray.length;i++){
             int k=numArray[i];
             longArray[i]=numArray[i];
-            for(int j=0;j<e;j++){
-//                int k=numArray[i];
+            for(int j=0;j<e-1;j++){
                 longArray[i]=(longArray[i]*k)%n;
             }
         }
@@ -59,9 +60,7 @@ public class RSA {
         for(int i=0;i<longArray.length;i++){
             long k=longArray[i];
             long l=longArray[i];
-            for(int j=0;j<d;j++){
-//                long k=longArray[i];
-                System.out.println("i:"+i+" l:"+l+" k:"+k);
+            for(int j=0;j<d-1;j++){
                 l=((l*k)%n);
             }
             intArray[i]=(int)l;
@@ -82,8 +81,31 @@ public class RSA {
         }
         return b;
     }
-    public static long gcdEx(long a, long b){
+    public static long[] gcdEx(long a, long b){
+        long x0 = 1, x1 = 0;
+        long y0 = 0, y1 = 1;
 
+        while (b != 0) {
+            long q = a / b;
+            long r = a % b;
+            long x2 = x0 - q * x1;
+            long y2 = y0 - q * y1;
+
+            a = b; b = r;
+            x0 = x1; x1 = x2;
+            y0 = y1; y1 = y2;
+        }
+
+        return new long[]{a, x0, y0};
+    }
+    public static long gcdEx2(long e, long a, long b){
+        for(int d=1;;d++){
+            long x;
+            x=a*d/b;
+            if((a*d-x*b)%e==1){
+                return d;
+            }
+        }
     }
     public static int[] getString(){
         BufferedReader br;
@@ -109,11 +131,36 @@ public class RSA {
         }
         return numArray;
     }
-    public static void showString(int[] numArray){//test
+    public static long[] getNum(){
+        BufferedReader br;
+        String s1;
+        br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter nums.");
+        s1 = "";
+        try {
+            s1 = br.readLine();
+        }catch(IOException e){
+            System.out.println("IOExeption occurred.");
+            return null;
+        }
+        String numArray[] = s1.split(",",0);
+        long array[] = new long[numArray.length];
+        for(int i=0;i<numArray.length;i++){
+            array[i]=Long.valueOf(numArray[i]);
+        }
+        return array;
+    }
+    public static void showString(int[] numArray){
         System.out.println("");
         for(int i=0;i<numArray.length;i++){
-//            System.out.print(alpha[numArray[i]]);
-            System.out.print(alpha[(numArray[i]>=ALPH_SUM)?ALPH_SUM-1:numArray[i]]);
+            System.out.print(alpha[(numArray[i]>=ALPH_SUM||numArray[i]<0)?ALPH_SUM-1:numArray[i]]);
+        }
+        System.out.println("");
+    }
+    public static void showString(long[] numArray){
+        System.out.println("");
+        for(int i=0;i<numArray.length;i++){
+            System.out.print(alpha[(numArray[i]>=ALPH_SUM||numArray[i]<0)?ALPH_SUM-1:(int)numArray[i]]);
         }
         System.out.println("");
     }
@@ -139,22 +186,19 @@ public class RSA {
             System.out.print(",");
         }
     }
-    public static long getPublicKey1(long p, long q){
-        return p*q;
+    public static long getPublicKey(long l){
+        for(long p=2;p<l;p++){
+            if(gcd(l,p)==1){
+                return p;
+            }
+        }
+        return 0;
     }
-//    public static long getPublicKey2(long p, long q){
-//        return lcm(p,q);
-//    }
     public static long getSecretKey(long e, long totient){
-        long d = 0;
-//        while(d<1000000000){
-//            if(d*e % totient == 1){
-//                System.out.println("d="+d);
-//                break;
-//            }
-//            d++;
-//        }
-        return d;
+        long d[]=gcdEx(e,totient);
+        System.out.println("d[0]:"+d[0]+" d[1]:"+d[1]+" d[2]"+d[2]);
+        if(d[2]>0)return d[2];
+        return d[1];
     }
     public static boolean millerRabin(long num){
         //Prime : True
@@ -183,14 +227,6 @@ public class RSA {
                 y = (y * y) % num;
                 t <<= 1;
             }
-//            if(y%num != 1){
-//                if(y%num != -1)break;
-//                for(;r>=0;r--){
-//                    y=y*y;
-//                    if(y%num != -1)break;
-//                }
-//                return false;
-//            }
             if(y != num-1 && (t & 1) == 0){
                 return false;
             }
@@ -207,5 +243,33 @@ public class RSA {
             d>>=1;
         }
         return result;
+    }
+    public static void generate(){
+        System.out.println("Generate prime number p and q");
+        Random random = new Random();
+        int randomWeight=100000;
+        int p=4;
+        while(!millerRabin(p)){
+            p=random.nextInt(randomWeight);
+        }
+        int q=4;
+        while(!millerRabin(q)){
+            q=random.nextInt(randomWeight);
+        }
+        if(p<0)p=p*(-1);
+        if(q<0)q=q*(-1);
+        //to see p and q
+        //System.out.println("p:"+p+"\nq:"+q);
+
+        System.out.println("Generate public key");
+        long e =lcm(p,q);
+        long pk = getPublicKey(e);
+        System.out.println("Public key:"+pk+"\nn:"+p*q);
+
+        System.out.println("Generate secret key:");
+        long sk = gcdEx2(e,pk,(p-1)*(q-1));
+        System.out.println("Secret key:"+sk);
+        p=0;q=0;pk=0;sk=0;
+        p=p+q+(int)sk+(int)pk;
     }
 }
