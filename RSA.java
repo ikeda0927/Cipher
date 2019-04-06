814c89ae2de7b04d6d31b2e1d143bdfde1c0cc8a
池田 康平 <ikeda.kohei4477@gmail.com>
2019-04-07 01:06:33 +0900

    High efficiency (fixed)

--- RSA.java
+++ RSA.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
+public class RSA {
     public static final int ALPH_SUM=53;
     static final char[] alpha = {'A','B','C','D','E','F','G','H','I','J',
             'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
